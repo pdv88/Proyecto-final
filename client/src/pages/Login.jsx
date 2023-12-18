d1b51ca857ff1d8f6af85634ecb48158c6da876a
Pablo De La Garza Vargas <54457168+pdv88@users.noreply.github.com>
2023-12-18 13:15:36 +0100

    try localhost 3306 on axios

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -20,7 +20,7 @@ function Login() {
     if (Object.keys(errorsTemp).length > 0) {
       setErrors(errorsTemp)
     } else {
-      axios.post("https://littlelemon-tkmu.onrender.com:3306/login", login)
+      axios.post("http://localhost:3306/login", login)
         .then((result) => {
           if (result.data.status === "success") {
             localStorage.setItem("user", JSON.stringify(login));
